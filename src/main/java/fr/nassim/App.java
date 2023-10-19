6014bceb232e0d5b9162487d09abedc3b25d1bc8
Zyvh <nassim59460@gmail.com>
2023-10-19 11:57:40 +0200

    Automatic restart if losing game

--- src/main/java/fr/nassim/App.java
+++ src/main/java/fr/nassim/App.java
@@ -15,22 +15,32 @@ public class App {
 
         System.out.println("Bienvenue dans le jeu de l'Oie !");
 
-        for (int i = 1; i <= 5; i++) {
-            lancer = deAleatoire.nextInt(6) + 1;
-            caseActuelle = caseActuelle + lancer;
-            nbCasesRestantes = nbCasesRestantes - lancer;
-            System.out.println("Lancer " + i + " : Vous avez fait " + lancer + ". Vous êtes actuellement sur la case "
-                    + caseActuelle + " (encore " + nbCasesRestantes + " cases)");
-            score = lancer + score;
+        while (score != objectif) {
+
+            score = 0;
+            caseActuelle = 0;
+            nbCasesRestantes = 20;
+
+            for (int i = 1; i <= 5; i++) {
+                lancer = deAleatoire.nextInt(6) + 1;
+                caseActuelle = caseActuelle + lancer;
+                nbCasesRestantes = nbCasesRestantes - lancer;
+                System.out
+                        .println("Lancer " + i + " : Vous avez fait " + lancer + ". Vous êtes actuellement sur la case "
+                                + caseActuelle + " (encore " + nbCasesRestantes + " cases)");
+                score = lancer + score;
+            }
+
+            if (score == objectif) {
+                System.out.println("Bravo tu as gagné !");
+            } else {
+                System.out.println("Vous avez perdu !");
+            }
+
         }
 
         System.out.println("Score final : " + score);
 
-        if (score == objectif) {
-            System.out.println("Bravo tu as gagné !");
-        } else {
-            System.out.println("Vous avez perdu !");
-        }
     }
 
 }
